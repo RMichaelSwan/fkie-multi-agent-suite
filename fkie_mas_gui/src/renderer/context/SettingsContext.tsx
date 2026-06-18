fca6c04510fc660243d9548ee7cde00530d49e08
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2026-06-18 10:20:21 +0200

    bump version of dependencies

--- fkie_mas_gui/src/renderer/context/SettingsContext.tsx
+++ fkie_mas_gui/src/renderer/context/SettingsContext.tsx
@@ -376,7 +376,7 @@ interface ISettingProvider {
 export const SettingsContext = createContext<ISettingsContext | null>(null);
 
 export function SettingsProvider({ children }: ISettingProvider): ReturnType<React.FC<ISettingProvider>> {
-  const MIN_VERSION_DAEMON = "5.1.1";
+  const MIN_VERSION_DAEMON = "5.7.1";
   const [changed, forceUpdate] = useReducer((x) => x + 1, 0);
   const [updatedArgs, forceUpdateArgs] = useReducer((x) => x + 1, 0);
   const [config, setConfig] = useLocalStorage<JSONObject>("SettingsContext:config", {});
